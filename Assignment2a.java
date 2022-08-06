01249b59d983e5efc19b4585f035723803882baa
Chandru <38471322+chandruswaminathan@users.noreply.github.com>
2022-08-06 21:23:30 +0530

    Add files via upload
    Assignment 1: Printing the data of two wheeler
    Assignment 2a: Checking whether the number is positive,negative or zero
    Assignment 2b: Converting the negative number to positive number

---
+++ Assignment2a.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class Assignment2a {
+		//To check whether the Number is Positive or Negative
+	public static void main(String[] args) {
+		
+		int num = -10;
+		
+		
+		if(num>0)
+		{
+			System.out.println("The number is positive");
+		}
+		else if(num<0)
+		{
+			System.out.println("The number is negative");
+		}
+		else
+		{
+			System.out.println("The number is neither positive nor negative");
+		}
+
+	}
+
+}
